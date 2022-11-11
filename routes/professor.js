6fd21edfff1e176349ea9e5352f674342b4aa2f9
faustocintra <professor@faustocintra.com.br>
2022-11-11 13:47:45 +0000

    (11/11) Configuração de autenticação

--- routes/professor.js
+++ routes/professor.js
@@ -1,16 +1,12 @@
 const express = require('express');
-
 const router = express.Router()
 const controller = require('../controllers/professor')
+const verifToken = require('../lib/verif_token')
 
-router.post('/', controller.create);
-
-router.get('/', controller.retrieve);
-
-router.get('/:id', controller.retriveOne);
-
-router.patch('/:id', controller.update)
-
-router.delete('/:id', controller.delete)
+router.post('/', verifToken, controller.create);
+router.get('/', verifToken, controller.retrieve);
+router.get('/:id', verifToken, controller.retriveOne);
+router.patch('/:id', verifToken, controller.update)
+router.delete('/:id', verifToken, controller.delete)
 
 module.exports = router
