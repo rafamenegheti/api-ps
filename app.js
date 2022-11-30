67ebac6e966a73357463bed8cc5ddf3899ee3550
faustocintra <professor@faustocintra.com.br>
2022-11-30 23:50:19 +0000

    (30/11) Alterada configuração do cors

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ var app = express();
 
 const cors = require('cors')
 app.use(cors({
-    origin: 'https://agoravai-fausto2.onrender.com',
+    // origin: 'https://agoravai-fausto2.onrender.com',
+    origin: true,
     credentials: true
 }))
 
