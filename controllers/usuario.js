f4876d7d41fc8d8caa6d3d55a7950fd286623841
Rafael Menegheti <rafaelmenegheti52@gmail.com>
2022-12-02 10:11:02 -0300

    fix: http cookie domain name

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -205,7 +205,7 @@ controller.login = async (req, res) => {
 
                 // Token retornando em um cookie seguro (HTTP only)
                 res.setHeader('Set-Cookie', 
-                    `app-data=${token}; Domain=agoravai-fausto.onrender.com; SameSite=None; Secure; Path=/; HttpOnly`).status(200).json({auth: true})
+                    `app-data=${token}; Domain=agoravai-rafael.onrender.com; SameSite=None; Secure; Path=/; HttpOnly`).status(200).json({auth: true})
             }
             else {  // Senha inválida
                 // HTTP 401: Unauthorized
